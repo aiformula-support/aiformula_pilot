d39378c8a8644dc54059ea4e1897f600ad679b70
SoraEbita <sora_ebita@jp.honda>
2025-07-28 18:47:31 +0900

    Add twist_mux submodules

--- launchers/sample_launchers/launch/all_pilot_nodes.launch.py
+++ launchers/sample_launchers/launch/all_pilot_nodes.launch.py
@@ -9,27 +9,48 @@ def generate_launch_description():
     # --- Command input from handle controller --- #
     handle_controller_joy = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            osp.join(get_package_share_directory("sample_launchers"),
-                     "launch/handle_controller_joy.launch.py"),
+            osp.join(
+                get_package_share_directory("sample_launchers"),
+                "launch/handle_controller_joy.launch.py",
+            ),
         ),
     )
     # --- Output velocity and angular velocity from handle controller --- #
     handle_controller_teleop = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            osp.join(get_package_share_directory("teleop_twist_handle_controller"),
-                     "launch/handle_controller_teleop.launch.py"),
+            osp.join(
+                get_package_share_directory("teleop_twist_handle_controller"),
+                "launch/handle_controller_teleop.launch.py",
+            ),
         ),
+    )
+    twist_mux = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            osp.join(
+                get_package_share_directory("sample_launchers"),
+                "launch/twist_mux.launch.py",
+            ),
+        ),
+        launch_arguments={
+            "use_rviz": "false",
+            "use_runtime_monitor": "false",
+        }.items(),
     )
 
     gui_webserver = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            osp.join(get_package_share_directory("gui_webserver"),
-                     "launch/gui_webserver.launch.py"),
+            osp.join(
+                get_package_share_directory("gui_webserver"),
+                "launch/gui_webserver.launch.py",
+            ),
         ),
     )
 
-    return LaunchDescription([
-        handle_controller_joy,
-        handle_controller_teleop,
-        gui_webserver,
-    ])
+    return LaunchDescription(
+        [
+            handle_controller_joy,
+            handle_controller_teleop,
+            twist_mux,
+            gui_webserver,
+        ]
+    )
